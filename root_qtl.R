8feecfba65d6fd1b63aeda30507cec8f32214dba
Tiffany Ho <tiaho@ucdavis.edu>
2014-09-14 11:14:31 -0700

    still not doing what I want it to

--- root_qtl.R
+++ root_qtl.R
@@ -13,4 +13,4 @@ ggplot(qtl) +
   geom_rect(aes(fill = as.factor(chr)),xmin = -Inf,xmax = Inf, ymin = -Inf,ymax = Inf,alpha = 0.01) +
   geom_hline(yintercept = 0.50, color = "red", size = 1) +
   theme(legend.position = "none") +
-  geom_histogram(aes(pos), binwidth = 0.1)
+  geom_histogram(aes(pos), binwidth = 1)

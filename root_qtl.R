29203b024ab0a38c3c25bb2a0eb888d8d4d873b6
Tiffany Ho <tiaho@ucdavis.edu>
2014-09-14 11:13:44 -0700

    still not doing what I want it to

--- root_qtl.R
+++ root_qtl.R
@@ -7,10 +7,10 @@ setwd("~/Desktop/Lab/Maloof Lab/QTL_Visualization/")
 
 qtl <- read.csv("chr_pos_lod_name.csv")
 
-ggplot(qtl, aes(pos, lod)) +
-  geom_line(size = 1.5) +
-  facet_grid(. ~ chr, scales = "free_x", space = "free_x") + 
+ggplot(qtl) +
+  geom_line(aes(pos, lod), size = 1.5) +
+  facet_grid(~ chr, scales = "free_x", space = "free_x") + 
   geom_rect(aes(fill = as.factor(chr)),xmin = -Inf,xmax = Inf, ymin = -Inf,ymax = Inf,alpha = 0.01) +
   geom_hline(yintercept = 0.50, color = "red", size = 1) +
-  theme(legend.position = "none") + 
-  scale_x_continuous(breaks = qtl$pos)
+  theme(legend.position = "none") +
+  geom_histogram(aes(pos), binwidth = 0.1)

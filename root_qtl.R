2af747268f574fca7f1ec1223960d818dcf09a55
Tiffany Ho <tiaho@ucdavis.edu>
2014-09-14 20:09:10 -0700

    plots the markers on the graph

--- root_qtl.R
+++ root_qtl.R
@@ -13,4 +13,4 @@ ggplot(qtl) +
   geom_rect(aes(fill = as.factor(chr)),xmin = -Inf,xmax = Inf, ymin = -Inf,ymax = Inf,alpha = 0.01) +
   geom_hline(yintercept = 0.50, color = "red", size = 1) +
   theme(legend.position = "none") +
-  geom_histogram(aes(pos), binwidth = 1)
+  geom_point(aes(x = pos, y = 0), color = 'blue', shape = 15)

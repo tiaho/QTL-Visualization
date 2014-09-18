a2feb269bcca8a2e267c3e7f3a2091efbda48826
Tiffany Ho <tiaho@ucdavis.edu>
2014-09-17 18:42:14 -0700

    backgrond colors are gray/white, lines are thinner, ticks instead of dots for marker position, improved on axis titles, ticks are labeled every 50 cM, removed margins between facets, angled x axis labels, removed white space between bottom of plot and x axis ticks

--- root_qtl.R
+++ root_qtl.R
@@ -1,18 +1,34 @@
 # root_qtl.R
 
 library(ggplot2)
+library(grid)
 
 setwd("~/Desktop/Lab/Maloof Lab/QTL_Visualization/")
 
 qtl <- read.csv("chr_pos_lod_name.csv")
 
+qtl$background.color <- "1"
+qtl$background.color[(qtl$chr %% 2) == 0] <- "0"
+
 ggplot(qtl) +
-  geom_line(aes(pos, lod), size = 1.5) +
   facet_grid(~ chr, scales = "free_x", space = "free_x") + 
-  geom_rect(aes(fill = as.factor(chr)),xmin = -Inf,xmax = Inf, ymin = -Inf,ymax = Inf,alpha = 0.01) +
+  geom_rect(aes(fill = background.color, xmin = -Inf, xmax = Inf, ymin = -0.02, ymax = Inf)) +
+  geom_line(aes(pos, lod), size = 1) +
   geom_hline(yintercept = 0.50, color = "red", size = 1) +
-  theme(legend.position = "none") +
-  geom_point(aes(x = pos, y = 0), color = 'blue') +
+  geom_segment(aes(x = pos, xend = pos, y = -0.005, yend = -0.015)) +
+  scale_x_continuous(breaks = seq(0, 1000, by = 50)) +
+  scale_y_continuous(expand = c(0, 0), limits = c(-0.02, max(qtl$lod * 1.02))) +
+  scale_fill_manual(values = c("0" = "gray90", "1" = "white")) +
+  theme(legend.position = "none",
+        axis.text.x = element_text(angle = 90),
+        axis.line=element_line(),
+        panel.margin = unit(0, "cm"),
+        panel.background = element_blank(),
+        panel.grid.major.y = element_blank(),
+        panel.grid.minor.y = element_blank()) +
   ggtitle("LOD Curves for QTLs") +
-  xlab("Genetic Distance in cM") +
-  ylab("LOD")
+  xlab("Genetic Position in cM") +
+  ylab("LOD Score")
+
+
+# if num of chromosome is odd, 1 == gray90. otherwise, 1 == white

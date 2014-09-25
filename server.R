f14fd148fcb7831dc60dd666cfcb7c698a2ad6ac
Mike Covington <mfcovington@gmail.com>
2014-09-24 18:52:25 -0700

    Clean up slider values (integers only)

--- server.R
+++ server.R
@@ -13,9 +13,9 @@ shinyServer(function(input, output) {
   # slider input
   output$slider <- renderUI({
     qtlChr <- subset(qtl, chr == input$chromosome)
-    max_pos <- max(qtlChr$pos, 1)
+    max_pos <- ceiling(max(qtlChr$pos, 1))
     sliderInput("region", label = h5("Display a region of the chromosome?"),
-     min = 0, max = max_pos, value = c(0, max_pos))
+     min = 0, max = max_pos, value = c(0, max_pos), step = 1)
   })
                            
   # plots the graph
